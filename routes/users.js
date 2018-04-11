2e0a717999c9f59f50145d1bdf5b611f2c645eab
jjrivasjr <jjrivasjr1@gmail.com>
2018-04-10 21:56:01 -0500

    new

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,29 @@ router.get('/register', (req, res) => {
 
 // Register Form POST
 router.post('/register', (req, res) => {
-	console.log(req.body);
-	res.send('register');
-})
+  let errors = [];
+
+  if(req.body.password != req.body.password2) {
+  	errors.push({text:'Passwords do not match'});
+  }	
+
+  if(req.body.password.length < 4){
+  	errors.push({text:
+  		'Password must be at least 4 characters'});
+  }
+
+  if(errors.length > 0){
+  	res.render('users/register', {
+  		errors: errors,
+  		name: req.body.name,
+  		email: req.body.email,
+  		password: req.body.password,
+  		password2: req.body.password2
+  	});
+  } else {
+  	res.send('Passed');
+  }
+
+});
 
 module.exports = router;
